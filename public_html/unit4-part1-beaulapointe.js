d2f51be3dddd82bf679d0533a860f41343347f3a
Owner <Owner@192.168.10.107>
2020-10-26 11:21:38 -0500

    The actual program

--- public_html/unit4-part1-beaulapointe.js
+++ public_html/unit4-part1-beaulapointe.js
@@ -2,5 +2,19 @@
  * ask for a store's daily sales for a week
  * calculate the total sales for the week
  */
+let dailySales = [];
 
+for (i = 1; i <= 7; i++) {
+    dailySales.push(prompt("Enter the sales for day " + i + ": "));
+    
+}
 
+i = 0;
+let sum = 0;
+
+while (i < 7) {
+    sum = sum + Number(dailySales[i]);
+    i++;
+}
+
+document.write("The total sales for the week is $" + sum);
